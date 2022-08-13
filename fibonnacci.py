6c214a154e31d4e8f5af492d67bbb47ff319a6b8
ChrisOmar43 <chrisomar08@hotmail.com>
2022-08-12 23:18:24 -0500

    Primer forma de resolver Fibonnacci

---
+++ fibonnacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+objetivo = int(input('Defina el mes que quiera calcular: '))
+
+print(fibonacci(objetivo))
