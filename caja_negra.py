85f72465cb0eeb2eaa64d0509135e7c24cf2bb0b
ChrisOmar43 <chrisomar08@hotmail.com>
2022-08-18 16:45:16 -0500

    Añado las pruebas en caja negra

---
+++ caja_negra.py
@@ -0,0 +1,35 @@
+import unittest
+
+def suma(num_1, num_2):
+    return abs(num_1) + num_2
+
+
+class CajaNegraTest(unittest.TestCase):
+
+    def test_suma_dos_positivos(self):
+        num_1 = 10
+        num_2 = 5
+
+        resultado = suma(num_1, num_2)
+
+        self.assertEqual(resultado, 15)
+
+    def test_suma_dos_negativos(self):
+        num_1 = -10
+        num_2 = -7
+
+        resultado = suma(num_1, num_2)
+
+        self.assertEqual(resultado, -17)
+
+    def test_multiplicacion(self):
+        num_1 = -5
+        num_2 = 3
+
+        resultado = multiplicacion(num_1, num_2)
+
+        self.assertEqual(resultado, -15)
+
+
+if __name__ == '__main__':
+    unittest.main()
